f3b770d5ecb7b8a2ed65dcf3714234d9eb4d3b34
TVDOMAIN\PetSjo <peter.sjostrand@tekniskaverken.se>
2017-05-17 12:11:58 +0200

    Lagt till StudensController vyerna till Students och bytt rubrik från Index till Student.

--- LerniaAttendanceStatistics/Models/Enrollment.cs
+++ LerniaAttendanceStatistics/Models/Enrollment.cs
@@ -1,11 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
 
+//namespace LerniaAttendanceStatistics.Models
+//{
+//    public class Enrollment
+//    {
+//    }
+//}
 namespace LerniaAttendanceStatistics.Models
 {
+    public enum Grade
+    {
+        A, B, C, D, F
+    }
+
     public class Enrollment
     {
+        public int EnrollmentID { get; set; }
+        public int CourseID { get; set; }
+        public int StudentID { get; set; }
+        public Grade? Grade { get; set; }
+
+        public Course Course { get; set; }
+        public Student Student { get; set; }
     }
 }
