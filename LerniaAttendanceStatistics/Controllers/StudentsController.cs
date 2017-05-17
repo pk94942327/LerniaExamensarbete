909605154d40994e9e306f74cc9c3f4579c7c57c
TVDOMAIN\PetSjo <peter.sjostrand@tekniskaverken.se>
2017-05-17 15:02:50 +0200

    Lagt till betygen i detaljvyn

--- LerniaAttendanceStatistics/Controllers/StudentsController.cs
+++ LerniaAttendanceStatistics/Controllers/StudentsController.cs
@@ -33,8 +33,15 @@ namespace LerniaAttendanceStatistics.Controllers
                 return NotFound();
             }
 
+            //var student = await _context.Students
+            //    .SingleOrDefaultAsync(m => m.ID == id);
+
             var student = await _context.Students
-                .SingleOrDefaultAsync(m => m.ID == id);
+                                    .Include(s => s.Enrollments)
+                                    .ThenInclude(e => e.Course)
+                                    .AsNoTracking()
+                                    .SingleOrDefaultAsync(m => m.ID == id);
+
             if (student == null)
             {
                 return NotFound();
